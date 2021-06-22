615d589e42e3f52d9c2ada4efef77d89d4751c04
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-06-22 19:00:52 -0400

    broken

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,67 +1,82 @@
-const express = require('express');
-const asyncHandler = require('express-async-handler');
+// Create a router here
+// Since we're doing database stuff, you'll want some kind of asyncHandler
+// Create the API route here
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User } = require('../../db/models');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-
+// Remember to export the router, too
+const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
-const validateLogin = [
-  check('credential')
+//import * as asyncHandler from 'express-async-handler';
+
+const validateSignup = [
+  check("email")
     .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
-  check('password')
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
   handleValidationErrors,
 ];
 
+router.get(
+  "/set-token-cookie",
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: "Demo-lition",
+      },
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+
+//! Sign up
 router.post(
-  '/',
-  validateLogin,
-  asyncHandler(async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.login({ credential, password });
-
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
+  "/",
+  validateSignup,
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
     await setTokenCookie(res, user);
 
     return res.json({
       user,
     });
-  }),
+  })
 );
 
-router.delete(
-  '/',
-  (_req, res) => {
-    res.clearCookie('token');
-    return res.json({ message: 'success' });
-  }
-);
+// // GET /api/restore-user
+// const { restoreUser } = require("../../utils/auth.js");
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
 
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
-    } else return res.json({});
-  }
-);
+// // GET /api/require-auth
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// const asyncHandler =
+//     (handler) =>
+//         (req, res, next) =>
+//             handler(req, res, next).catch(next);
+
+// Take a second to import the database stuff you'll need
+// Here's where you'd also import other middleware
 
 module.exports = router;
