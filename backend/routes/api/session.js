ab82d974aa9d4f2b89dc376161502f7a3e374b38
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-09-19 23:41:31 -0400

    session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,7 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User } = require('../../db/models/user');
+const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
